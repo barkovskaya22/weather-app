313030908e852e4a762d8deac8618636c500b59e
Ornella Barca <33401351+barkovskaya22@users.noreply.github.com>
2021-08-01 16:19:40 -0400

    first com
    fisst com

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const api = {
+  key: "202f7373c7a4dd632de52bc0c9cf00c2 ",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
